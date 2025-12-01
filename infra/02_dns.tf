560c4531ff4386b056351341867bccd78c7af6c4
Adelin Niculae <43118261+adelinn@users.noreply.github.com>
2025-12-01 02:22:35 +0200

    make dns record longer

--- infra/02_dns.tf
+++ infra/02_dns.tf
@@ -9,6 +9,6 @@ resource "google_dns_record_set" "frontend" {
   name         = "app.${google_dns_managed_zone.main.dns_name}"
   managed_zone = google_dns_managed_zone.main.name
   type         = google_firebase_hosting_custom_domain.main.required_dns_updates[0].desired[0].records[0].type
-  ttl          = 300
+  ttl          = 3600
   rrdatas      = ["${google_firebase_hosting_custom_domain.main.required_dns_updates[0].desired[0].records[0].rdata}."]
 }
