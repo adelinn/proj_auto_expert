84e4bc68d30177c32c91d872c0f64870332fddcf
Alex <157403643+Gypsanu@users.noreply.github.com>
2025-12-15 03:36:04 +0200

    Am adauagat până la 5 întrebari

--- backend/questions.js
+++ backend/questions.js
@@ -20,9 +20,40 @@ const questions = [
       { id: "c", text: "Ignori" }
     ],
     correctOptionId: "b"
+  },
+  {
+    id: 3,
+    text: "Care este limita maximă de viteză în localități?",
+    image: null,
+    options: [
+      { id: "a", text: "50 km/h" },
+      { id: "b", text: "60 km/h" },
+      { id: "c", text: "80 km/h" }
+    ],
+    correctOptionId: "a"
+  },
+  {
+    id: 4,
+    text: "Ce indicație oferă acest semn?",
+    image: null,
+    options: [
+      { id: "a", text: "Drum cu prioritate" },
+      { id: "b", text: "Cedează trecerea" },
+      { id: "c", text: "Oprire" }
+    ],
+    correctOptionId: "a"
+  },
+  {
+    id: 5,
+    text: "Ce trebuie să faci când vezi acest indicator?",
+    image: null,
+    options: [
+      { id: "a", text: "Drum cu prioritate" },
+      { id: "b", text: "Cedează trecerea" },
+      { id: "c", text: "Oprire" }
+    ],
+    correctOptionId: "a"
   }
 ];
-//Acea am facut eu întrebarie  pentru chestionaruk auto
-
 
 module.exports = questions;
