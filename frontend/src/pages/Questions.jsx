c6043f8e1db42f9a876cf255afa39a070b736b6a
Adelin Niculae <43118261+adelinn@users.noreply.github.com>
2025-12-19 05:39:48 +0200

    fix linter err and others

--- frontend/src/pages/Questions.jsx
+++ frontend/src/pages/Questions.jsx
@@ -1,8 +1,25 @@
+import Pin from "../components/Pin";
+import QuizOption from "../components/QuizOption";
+import QuizOptions from "../components/QuizOptions";
+
 export default function Questions() {
   return (
-    <main style={{padding: '2rem', color: 'var(--color-beige-100)'}}>
-      <h1>Pagina de întrebări</h1>
-      <p className="subtitle">Aceasta este pagina de întrebări. Aici vor apărea testele pentru categoria selectată.</p>
-    </main>
+    <section className="-mt-8 relative">
+      {/* Content with relative positioning */}
+      <div className="relative z-10 color-beige-100 p-2">
+        <Pin />
+        <Pin />
+        <Pin />
+        <h1>Întrebare rapidă...</h1>
+        <QuizOptions size="lg" imageSrc="" imageAlt="">
+          <QuizOption id="opt-a">
+            Aceasta este pagina de întrebări. Aici vor apărea testele pentru
+            categoria selectată.
+          </QuizOption>
+          <QuizOption id="opt-b">Weeeeeeeeeeeeee.</QuizOption>
+          <QuizOption id="opt-c">Weeeeeeeeeeeeee.</QuizOption>
+        </QuizOptions>
+      </div>
+    </section>
   );
 }
